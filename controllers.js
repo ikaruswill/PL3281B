66ea4503ff68a1e6e3d0c955f8d99bb62b393f96
IkarusWill <qwerty.willy@gmail.com>
2015-03-21 04:49:41 +0800

    Start page attained.

--- controllers.js
+++ controllers.js
@@ -1,65 +1,103 @@
 // Create module for the experiment
 var mod = angular.module('psych', ['ngRoute']);
 
-mod.factory('psychService', function() {
-    var sessionParams = {
-        sessionNo: '0',
-        testType: 'NULL'
-    }
+mod.service('psychService', function() {
 
-    return {
-        getSessionParams: function() {
-            return sessionParams;
-        }
-    }
+    this.sessionParams = {
+        sessionNo: 'NIL',
+        testType: 'NIL'
+    };
+
+
+
 });
 
 
 //Set up mappings
 function routeConfig($routeProvider) {
     $routeProvider.when('/', {
-        controller: 'sessionFormController',
-        controllerAs: 'formCon',
+        controller: 'sessionInitController',
+        controllerAs: 'initCon',
         templateUrl: 'session-request.html'
     }).when('/ins-common', {
-        controller: 'sessionFormController',
-        controllerAs: 'formCon',
+        controller: 'sessionInitController',
+        controllerAs: 'initCon',
         templateUrl: 'ins-common.html'
-    }).when('/ins-colour', {
-        templateUrl: 'ins-colour.html'
     }).when('/ins-order', {
+        controller: 'orderController',
+        controllerAs: 'oCon',
         templateUrl: 'ins-order.html'
+    }).when('/ins-colour', {
+        controller: 'colourController',
+        controllerAs: 'cCon',
+        templateUrl: 'ins-colour.html'
+    }).when('/order/practice/start', {
+        controller: 'startController',
+        controllerAs: 'oCon',
+        templateUrl: 'start.html'
+    }).when('/colour/practice/start', {
+        controller: 'startController',
+        controllerAs: 'cCon',
+        templateUrl: 'start.html'
     })
 }
 
 mod.config(routeConfig);
 
-mod.controller('sessionFormController', ['psychService', '$location', function(psychService, $location){
+mod.controller('sessionInitController', ['psychService', '$location', function(psychService, $location){
         var vm = this;
-        var sessionParams = psychService.getSessionParams();
+        var sessionParams = psychService.sessionParams;
         vm.sessionNoInput = '';
 
         vm.parseSessionNoInput = function() {
-            if(vm.sessionNoInput % 2 == 0) {
-                sessionParams.testType = 'colour';
-            } else if(vm.sessionNoInput % 2 != 0){
+            if(vm.sessionNoInput % 2 == 1) {
                 sessionParams.testType = 'order';
+            } else if(vm.sessionNoInput % 2 == 0){
+                sessionParams.testType = 'colour';
             }
             sessionParams.sessionNo = vm.sessionNoInput;
             $location.path('/ins-common');
         };
 
         vm.divergeInsRedirect = function() {
-            if(sessionParams.testType == 'colour') {
-                $location.path('/ins-colour');
-            } else if(sessionParams.testType == 'order'){
+            if(sessionParams.testType == 'order'){
                 $location.path('/ins-order');
+            }else if(sessionParams.testType == 'colour') {
+                $location.path('/ins-colour');
             }
         }
     }]
 );
 
-mod.controller('commonDivergeController', ['psychService', '$location', function(psychService, $location) {
+mod.controller('startController', ['psychService','$location', '$timeout', '$scope', function(psychService, $location, $timeout, $scope) {
+    var testType = psychService.sessionParams.testType;
+    var redirect = function() {
+        if(testType == 'colour') {
+            $location.path('/colour/practice/1');
+        } else if(testType == 'order') {
+            $location.path('/order/practice/1');
+        }
+    };
+    var timer = $timeout(redirect, 1000);
+
+    $scope.$on('$destroy', function() {
+        $timeout.cancel(timer);
+    })
+}]);
+
+mod.controller('orderController', ['psychService', '$location', function(psychService, $location) {
     var vm = this;
 
+    vm.beginPractice = function() {
+        $location.path('/order/practice/start');
+    }
+
+}]);
+
+mod.controller('colourController', ['psychService', '$location', function(psychService, $location) {
+    var vm = this;
+
+    vm.beginPractice = function() {
+        $location.path('/colour/practice/start');
+    }
 }]);
